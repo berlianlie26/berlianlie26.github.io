b359dd9dc6f907f1f8e1bfa2c70b57380b8799ab
berlianlie26 <berlianlie@gmail.com>
2019-11-21 13:10:48 +0700

    Berulangkalii

---
+++ precache-manifest.b0ae3133d6e906912007c64ed88f2ac1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b831b8940d5e6f9499fd",
+    "url": "/css/about.6900382a.css"
+  },
+  {
+    "revision": "7b53ef3f14151c87ab23",
+    "url": "/css/chunk-vendors.218b7d4c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e3b0c699cc5480370d1563ec99f9c848",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b831b8940d5e6f9499fd",
+    "url": "/js/about.bd4a45cf.js"
+  },
+  {
+    "revision": "2e0a95614fee430daffd",
+    "url": "/js/app.190dc95c.js"
+  },
+  {
+    "revision": "7b53ef3f14151c87ab23",
+    "url": "/js/chunk-vendors.fe1498e9.js"
+  },
+  {
+    "revision": "a03e2cbc63cf49ffbda1633b2db96471",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
